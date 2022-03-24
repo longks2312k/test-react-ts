1b9ad2949249006e50acc146085d866f45024c6e
longks2312k <longks2312k@gmail.com>
2022-03-24 16:13:12 +0700

    Done S1

--- src/component/WelcomMessage.tsx
+++ src/component/WelcomMessage.tsx
@@ -2,16 +2,19 @@ import { Box } from '@material-ui/core';
 import React from 'react';
 
 interface WelcomeMessageProps {
-    position: string
-    country?: string
+  position: string;
+  country?: string;
 }
 
-const WelcomeMessage = ({ position, country = 'VietNam' }: WelcomeMessageProps) => {
-    return (
-        <Box mb={1}>
-            Welcome {position} from {country}!
-        </Box>
-    );
+const WelcomeMessage = ({
+  position,
+  country = 'VietNam'
+}: WelcomeMessageProps) => {
+  return (
+    <Box mb={1}>
+      Welcome {position} from {country}!
+    </Box>
+  );
 };
 
 export default WelcomeMessage;
